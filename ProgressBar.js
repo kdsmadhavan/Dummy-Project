74e2d3bcee60bd6d3ea6c2254458df02e63f8c23
kdsmadhavan <142239693+kdsmadhavan@users.noreply.github.com>
2026-03-14 11:37:14 +0530

    Create ProgressBar.js

---
+++ ProgressBar.js
@@ -0,0 +1,16 @@
+export default function ProgressBar(){
+
+  return(
+
+    <div className="w-full p-3 bg-black border-t border-gray-800">
+
+      <div className="h-3 bg-gray-800 rounded">
+
+        <div className="h-3 w-1/4 bg-cyan-500 rounded"/>
+
+      </div>
+
+    </div>
+
+  )
+}
