6164aba76ba39fc9912b01a276d4178b6aaa79b6
kdsmadhavan <142239693+kdsmadhavan@users.noreply.github.com>
2026-03-14 11:35:07 +0530

    Create page.js

---
+++ page.js
@@ -0,0 +1,18 @@
+import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
+import FeatureSection from "../components/FeatureSection"
+
+export default function Home() {
+
+  return (
+    <main>
+
+      <Navbar/>
+
+      <Hero/>
+
+      <FeatureSection/>
+
+    </main>
+  )
+}
